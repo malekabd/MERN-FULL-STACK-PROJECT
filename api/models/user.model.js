a9aaefdbd9ba13519a579528c99172376e8b1870
malekabd <61131777+malekabd@users.noreply.github.com>
2023-10-14 08:29:05 +0300

    create user modal

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true } //used to record the creation and edit time
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
