e0f2edf211c0c958656a2fb0e0db0e1976e04c1a
malekabd <61131777+malekabd@users.noreply.github.com>
2023-10-15 12:05:01 +0300

    create a sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -8,6 +9,38 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid credential !"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc; //this destructering is to sen the uer data without the encrypted password
+    res
+      .cookie("access_token", token, {
+        //this is the way how to define a session
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
+    /* res.cookie("access_token", token, {// this is the way how to define a normal cookie
+      httpOnly: true,
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    })  .status(200)
+      .json(validUser);; */
+    //httponly means third party apps can use this cookie
+    //res.status(201).json("User created successfully!");
   } catch (error) {
     next(error);
   }
