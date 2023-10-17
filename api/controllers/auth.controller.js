e4333953fd6d741e2d52bb6bc9499ae7d3e3d9ce
malekabd <61131777+malekabd@users.noreply.github.com>
2023-10-17 23:09:34 +0300

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -81,3 +81,11 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json({ message: "Successfully signed out!" });
+  } catch (error) {
+    next(error);
+  }
+};
